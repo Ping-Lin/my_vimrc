04a449c6d361c7ec49afe50882eb8f6acccd7c5f
Ping-Lin <billy3962@hotmail.com>
2020-01-26 17:34:16 +0800

    mac gdf support, copy the file ourself
    since packaging tools are totally different, Eg. apt, brew
    this script is mainly for linux.

--- install.sh
+++ install.sh
@@ -14,6 +14,19 @@ install_vimrc()
     esac
 }
 
+install_ycm()
+{
+    read -r -p "Install new ~/.ycm_extra_conf.py, continue(y/n): " INSTALL_YCM
+    case $INSTALL_YCM in
+        Y|y)
+            python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --ts-completer
+            /usr/bin/install -c -m755 ycm_extra_conf.py ~/.ycm_extra_conf.py
+            ;;
+        N|n|*)
+            ;;
+    esac
+}
+
 install_gdftool()
 {
     read -r -p "Install new gdftool (linux only), continue(y/n): " INSTALL_GDFTOOL
@@ -22,19 +35,26 @@ install_gdftool()
     case $INSTALL_GDFTOOL in
         Y|y)
             /usr/bin/install -c -m755 ~/.vim/tool/gdf /usr/local/bin/gdf
-            /usr/bin/install -c -m755 ~/.vim/tool/gdf_wrapper /usr/local/bin/gdf_wrapper
             ;;
         N|n|*)
             ;;
     esac
 }
 
-install_ycm()
+install_linux_used()
 {
-    read -r -p "Install new ~/.ycm_extra_conf.py, continue(y/n): " INSTALL_YCM
-    case $INSTALL_YCM in
+    read -r -p "Install other config (linux only), continue(y/n): " INSTALL_LINUX_OTHERS
+
+    case $INSTALL_LINUX_OTHERS in
         Y|y)
-            /usr/bin/install -c -m755 ycm_extra_conf.py ~/.ycm_extra_conf.py
+            # Set shellcheck
+            apt install shellcheck
+
+            # Set YouCompleteMe
+            apt install build-essential cmake python3-dev
+            apt install npm
+            # update npm
+            npm install npm -g
             ;;
         N|n|*)
             ;;
@@ -54,14 +74,4 @@ done
 install_vimrc
 install_gdftool
 install_ycm
-
-# Set shellcheck
-apt install shellcheck
-
-# Set YouCompleteMe
-apt install build-essential cmake python3-dev
-apt install npm
-# update npm
-npm install npm -g
-python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --ts-completer
-
+install_linux_used
